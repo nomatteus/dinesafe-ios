2756caf6c1885ad2e784452109c7582d573f551e
nomatteus <matt@ruten.ca>
2013-02-04 07:01:40 -0500

    Fix inspection cell display bug (wasn't updating). Use setNeedsDisplay, now that my updateCellContent method is not used anymore.

--- Dinesafe/DSFInspectionCell.m
+++ Dinesafe/DSFInspectionCell.m
@@ -19,43 +19,6 @@
     // [self setBackgroundColor:[UIColor grayColor]];
 }
 
-- (void)updateCellContent
-{
-
-//    if ([self.inspection.status isEqualToString:@"Conditional Pass"]) {
-//        self.status.text = @"Conditional";
-//    } else {
-//        self.status.text = self.inspection.status;
-//    }
-//    
-//    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-//    [dateFormatter setDateStyle:NSDateFormatterLongStyle];
-//    self.date.text = [dateFormatter stringFromDate:self.inspection.date];
-//    
-//    self.statusBox.backgroundColor = [self.inspection colorForStatusAtPositionUIColor:1];
-//
-//    float offsetY = 76;
-//    float infractionHeight = 50;
-//    for (DSFInfraction *infraction in self.inspection.infractions) {
-//        // TODO: All the work done in this for-in loop is causing scrolling
-//        //      to lag, because this is done every time a cell is viewed.
-//        //    TODO: move the bulk of this somewhere else... not sure where....
-//        CGRect severityFrame = CGRectMake(16, offsetY, 87, infractionHeight);
-//        UITextView *severity = [[UITextView alloc] initWithFrame:severityFrame];
-//        severity.text = infraction.severity;
-//        severity.editable = NO;
-//       
-//        CGRect detailsFrame = CGRectMake(104, offsetY, 208, infractionHeight);
-//        UITextView *details = [[UITextView alloc] initWithFrame:detailsFrame];
-//        details.text = infraction.details;
-//        details.editable = NO;
-//        
-//        [self.infractionsView addSubview:severity];
-//        [self.infractionsView addSubview:details];
-//        offsetY += infractionHeight;
-//    }
-}
-
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
--- Dinesafe/DSFInspectionTableViewController.m
+++ Dinesafe/DSFInspectionTableViewController.m
@@ -184,7 +184,7 @@
     // TODO: Move inspectionIndex and cell order/etc to a consolidated place, i.e. figure out cell heights and orders once, and then return that instead of doing the calculations on every cell load, Also we're calculating inspectionIndex in 2 different places and that's confusing to update. Also, I'm very tired right now and can't articulate myself very well.
     int inspectionIndex = self.establishment.inspections.count - 1 - indexPath.row + 2;  // reverse order
     cell.inspection = self.establishment.inspections[inspectionIndex];
-    [cell updateCellContent];
+    [cell setNeedsDisplay];
     return cell;
 }
 
