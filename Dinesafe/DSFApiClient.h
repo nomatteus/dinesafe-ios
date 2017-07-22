291a17a13ba57d872a8773ef2417ccd02e67bb53
nomatteus <matt@ruten.ca>
2017-07-22 15:44:47 -0400

    Fix layout and constraints

--- Dinesafe/DSFApiClient.h
+++ Dinesafe/DSFApiClient.h
@@ -8,7 +8,7 @@
 
 #import "AFNetworking.h"
 
-#define DINESAFE_API_BASE_URL @"http://dinesafe.to/api/1.0/"
+#define DINESAFE_API_BASE_URL @"https://dinesafe.to/api/1.0/"
 //#define DINESAFE_API_BASE_URL @"http://dinesafe.dev/api/1.0/"
 //#define DINESAFE_API_BASE_URL @"http://localhost:3000/api/1.0/"
 
